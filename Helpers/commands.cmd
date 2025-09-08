9fc36f4f283c6690407976cba03ee7c8f28d3401
leefraine <leefrainee@gmail.com>
2025-09-08 15:43:09 +0530

    Update commands.cmd

--- Helpers/commands.cmd
+++ Helpers/commands.cmd
@@ -1,1 +1,20 @@
 icacls "C:\inetpub\wwwroot\yourfolder\uploads" /grant "IIS AppPool\YourAppPoolName":(M)
+
+
+
+
+
+# Create the credential object
+$securePassword = ConvertTo-SecureString "PlainTextPassword" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ("username", $securePassword)
+
+# Start the BITS transfer upload with custom headers and credentials
+Start-BitsTransfer `
+  -Source "C:\Path\to\file.bin" `
+  -Destination "http://<IIS-SERVER>/upload.asp" `
+  -TransferType Upload `
+  -Credential $credential `
+  -Authentication Basic `
+  -CustomHeaders @("BITS_POST: 1", "User-Agent: Microsoft BITS/7.8")
+
+
